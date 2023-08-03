48ec4bd051307a2304c6018407edf804a8cdbd89
falsumit <brooks516@outlook.com>
2023-08-03 14:22:09 -0400

    completed exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(heyString, testLoop) {
+    if (heyString === '') {
+        return heyString = ''
+    } 
+    
+    for (let i = 0; i <= testLoop; i++) {
+        heyString = 'hey'.repeat(i);
+    } 
+    
+    if (testLoop >= 0) {
+        return heyString
+    } else if (testLoop < 0) {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(oldString) {
+    if (oldString === '') {
+        return ''
+    }
+    let stringArray = oldString.split("");
+    stringArray.reverse();
+    let newString = stringArray.join("");
+    return newString
 };
-
 // Do not edit below this line
 module.exports = reverseString;
