61b35e23bbe9c4457f813584df6bb75d3faa5de2
falsumit <brooks516@outlook.com>
2023-08-03 18:05:05 -0400

    Made some progress on exercise 4 but need a break to reset my mind

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(newArray = [1, 2, 3, 4, 5], values) {
+    
+    newArray.splice(value - 1, 1);
+    console.log(newArray);
+};
 
+removeFromArray();
+/*
+const removeFromArray = function(newArray, value) {
+    newArray.splice(value - 1, 1);
+    return newArray
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
+*/
