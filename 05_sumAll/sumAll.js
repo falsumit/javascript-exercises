4b91d9e6845a72e1ccdb0c6c29960d766201bfd4
falsumit <brooks516@outlook.com>
2023-08-04 13:58:09 -0400

    finished ex.5 without looking at the solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 ||
+        typeof a !== "number" || isNaN(a) || 
+        typeof b !== "number" || isNaN(b)) {
+        return 'ERROR'
+    } else {
+        let num = [a, b];
+        num.sort(function(a, b){return a-b});
+        let lowVal = num[0];
+        let highVal = num[1];
+        num.pop();
 
+        for (let i = lowVal + 1; i <= highVal; i++) {
+            num.push(i);
+        }
+        let result = num.reduce((accumulator, currentValue) => accumulator + currentValue);
+        return result
+    }
 };
 
 // Do not edit below this line
