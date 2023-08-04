5e6191c047edafe3a85546d710b77669e7d0ed1a
falsumit <brooks516@outlook.com>
2023-08-04 12:11:09 -0400

    completed ex. 4 with help, but finally understood the issue and solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,6 @@
-const removeFromArray = function(newArray = [1, 2, 3, 4, 5], values) {
-    
-    newArray.splice(value - 1, 1);
-    console.log(newArray);
-};
-
-removeFromArray();
-/*
-const removeFromArray = function(newArray, value) {
-    newArray.splice(value - 1, 1);
-    return newArray
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
-//module.exports = removeFromArray;
-*/
+module.exports = removeFromArray;
