81e1a75f7c44ce3c156c6aa2745171cdb475ab5a
falsumit <brooks516@outlook.com>
2023-08-04 16:45:32 -0400

    finished ex.6 without checking solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year / 100) && !Number.isInteger(year / 400)) {
+        return false
+    } else if (Number.isInteger(year / 4 || year / 400)) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
